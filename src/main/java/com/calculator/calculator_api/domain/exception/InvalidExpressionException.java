226bca8a4b633ec726b25a84e1e2c78b4070dc10
Federico Cantore <f.cantore85@gmail.com>
2026-06-18 17:57:15 +0200

    Revert "Feature/evaluate operations"

--- src/main/java/com/calculator/calculator_api/domain/exception/InvalidExpressionException.java
+++
@@ -1,7 +0,0 @@
-package com.calculator.calculator_api.domain.exception;
-
-public class InvalidExpressionException extends RuntimeException {
-    public InvalidExpressionException() {
-        super("Invalid mathematical expression");
-    }
-}
--- src/main/java/com/calculator/calculator_api/domain/exception/NegativeSquareRootException.java
+++
@@ -1,7 +0,0 @@
-package com.calculator.calculator_api.domain.exception;
-
-public class NegativeSquareRootException extends RuntimeException {
-    public NegativeSquareRootException() {
-        super("Cannot calculate square root of a negative number");
-    }
-}
